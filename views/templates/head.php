f81f002ce4b8100dcfe58dc8139acb4f6d7a83d7
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2015-05-07 13:20:56 -0500

    templated

--- views/templates/head.php
+++ views/templates/head.php
@@ -26,4 +26,14 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <link href='/binary/themes/CUSTOM/DEPUY/DPY_SYN_INST/scripts/ie8.css' rel='stylesheet'>
   <![endif]-->
+  <script>
+    (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
+    ga("create", "UA-58518993-1", "auto");
+    ga("send", "pageview");
+  </script>
+  <script type="text/javascript" src="../../js/source/main.js"></script>
+  <script>initDSI()</script>
 </head>
---
+++ views/templates/mobile-header.php
@@ -0,0 +1,50 @@
+<header class="mobile_header visible-xs-block clearfix">
+  <div class="white-bg">
+    <a href="/" class="logoLink">
+    </a>
+    <h1 class="logo">
+      Depuy Synthes Institute - advancing education and research
+    </h1>
+    <div id="mobileDropdown" class="dropdown mobile_nav_button pull-left">
+      <button class="btn btn-info btn-block dropdown-toggle" type="button" data-toggle="dropdown">
+        <span id="thatOne"></span>
+        <span id="mobileNavIcon" class="glyphicon icon-caret-down">
+        </span>
+      </button>
+      <ul class="dropdown-menu mobile-nav">
+
+        <li class=""><a href="/profile" class=""><span>My Profile</span><span class="pull-right glyphicon icon-profile"></span></a></li>
+        <li class=""><a href="/favorites" class=""><span>My Favorites</span><span class="pull-right glyphicon icon-favorites"></span></a></li>
+        <li class=""><a href="views/my-assignments-resident/index.php" class=""><span>My Assignments</span><span class="pull-right glyphicon icon-assignments"></span></a></li>
+
+        <li class=""><a href="/"><span>Home</span><span class="pull-right glyphicon icon-home"></span></a></li>
+        <li class=""><a href="/knowledge-center" class=""><span>Knowledge Center</span><span class="pull-right glyphicon icon-knowledge-center"></span></a></li>
+        <li class=""><a href="/redemption-center" class=""><span>Redemption Center</span><span class="pull-right glyphicon icon-redemption-center"></span></a></li>
+        <li class=""><a href="/graduation-resources" class=""><span>Graduation Resources</span><span class="pull-right glyphicon icon-graduation-resources"></span></a></li>
+        <li class=""><a href="/external-resources" class=""><span>External Resources</span><span class="pull-right glyphicon icon-external-resources"></span></a></li>
+        <li class=""><a href="/calendar" class=""><span>Course Calendar</span><span class="pull-right glyphicon icon-calendar"></span></a></li>
+        <li class=""><a href="/faq" class=""><span>FAQs</span><span class="pull-right glyphicon icon-faq"></span></a></li>
+        <li class=""><a href="/contact" class=""><span>Contact Us</span><span class="pull-right glyphicon icon-contact"></span></a></li>
+
+        <li class="mobile-nav-break"></li>
+        <li class="sub-nav"><a href="/surgeon-education"><span>Surgeon Education</span><span class="second glyphicon icon-caret-right"></span></a></li>
+        <li class="sub-nav"><a href="/nurse-education"><span>Nurse Education<span><span class="second glyphicon icon-caret-right"></span></a></li>
+        <li class="sub-nav"><a href="/veterinary"><span>Veterinary Education<span><span class="second glyphicon icon-caret-right"></span></a></li>
+
+      </ul>
+    </div>
+  </div>
+  <form class="form-inline" action="/search?" method="get" id="mblSearchForm" onsubmit="return siteSearch(this);">
+    <input type="hidden" name="page" value="0" />
+    <input type="hidden" name="pmid" value="c0a80237b1f9d2b441fd3d624d14c12e" />
+    <div class="mobile_search_button pull-right">
+      <button class="btn btn-info btn-search" type="button" id="mblSrchBtn">
+        <span class="glyphicon icon-search">
+        </span>
+      </button>
+    </div>
+    <div class="nav-search hide" id="mblNavSrch">
+      <input id="search1" name="searchData" type="text" value="" class="typeahead form-control" placeholder="search" onfocus="this.value='';">
+    </div>
+  </form>
+</header>
