edc0d91d2f36ba86ae88e6efdeceac77f550f1db
Sarah Spencer <sarah@12southmusic.com>
2015-05-11 15:09:18 -0500

    copying over resident changes

--- views/my-assignments/resident.php
+++ views/my-assignments/resident.php
@@ -15,7 +15,7 @@
 
         <div id="smt_col1Wrapper">
           <div id="smt_col1">
-            <div class="modSpacer">
+            <div class="modSpacer current-assignments">
               <div class="row">
                 <div class="col-xs-12">
                   <h1>My Current Assignments</h1>
@@ -23,132 +23,144 @@
               </div>
               <div class="row">
                 <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
-                  <h5>Due Date</h5>
+                  <h5>
+                    <span class="assignment-heading">
+                      Due Date
+                    </span>
+                  </h5>
                 </div>
                 <div class="col-xs-6 col-sm-8 col-md-10 col-lg-10">
-                  <h5>Assignment</h5>
+                  <h5>
+                    <span class="assignment-heading">
+                      Assignment
+                    </span>
+                  </h5>
                 </div>
                 <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
-                  <h5>Completed</h5>
+                  <h5>
+                    <span class="assignment-heading">
+                      Completed
+                    </span>
+                  </h5>
                 </div>
               </div>
               <hr>
               <div class="row">
                 <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
-                  <div class="row">
-                    <div class="col-xs-12">
-                      JUN
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-xs-12">
-                      19
+                  <div class="pull-left">
+                    <div class="calendar">
+                      <p class="calendar_month">JUN</p>
+                      <p class="calendar_date">19</p>
                     </div>
                   </div>
                 </div>
                 <div class="col-xs-6 col-sm-8 col-md-10 col-lg-10">
                   <div class="row">
                     <div class="col-xs-12">
-                      <a href="show-ordered.php">Name Of Resident Assignment</a>
+                      <h3 class="course_description">
+                        <a href="show-ordered.php">Name Of Resident Assignment</a>
+                      </h3>
                     </div>
                   </div>
                   <div class="row">
                     <div class="col-xs-12">
-                      2 PDFs, 3 Videos, 1 Podcast, 1 Event, 1 eLearning
+                      <p class="course_contents course_location">
+                        2 PDFs, 3 Videos, 1 Podcast, 1 Event, 1 eLearning
+                      </p>
                     </div>
                   </div>
                 </div>
-                <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
+                <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1 percentage-complete">
                   38%
                 </div>
               </div>
               <hr>
               <div class="row">
                 <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
-                  <div class="row">
-                    <div class="col-xs-12">
-                      DEC
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-xs-12">
-                      27
+                  <div class="pull-left">
+                    <div class="calendar">
+                      <p class="calendar_month">DEC</p>
+                      <p class="calendar_date">27</p>
                     </div>
                   </div>
                 </div>
                 <div class="col-xs-6 col-sm-8 col-md-10 col-lg-10">
                   <div class="row">
                     <div class="col-xs-12">
-                      <a href="show-ordered.php">Name Of Resident Assignment</a>
+                      <h3 class="course_description">
+                        <a href="show-ordered.php">Name Of Resident Assignment</a>
+                      </h3>
                     </div>
                   </div>
                   <div class="row">
                     <div class="col-xs-12">
+                    <p class="course_contents course_location">
                       2 PDFs, 3 Videos
+                    </p>
                     </div>
                   </div>
                 </div>
-                <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
+                <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1 percentage-complete">
                   20%
                 </div>
               </div>
               <hr>
               <div class="row">
                 <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
-                  <div class="row">
-                    <div class="col-xs-12">
-                      DUE
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-xs-12">
-                      NA
+                  <div class="pull-left">
+                    <div class="calendar">
+                      <p class="calendar_month">DUE</p>
+                      <p class="calendar_date">NA</p>
                     </div>
                   </div>
                 </div>
                 <div class="col-xs-6 col-sm-8 col-md-10 col-lg-10">
                   <div class="row">
                     <div class="col-xs-12">
-                      <a href="show-ordered.php">Name Of Resident Assignment</a>
+                      <h3 class="course_description">
+                        <a href="show-ordered.php">Name Of Resident Assignment</a>
+                      </h3>
                     </div>
                   </div>
                   <div class="row">
                     <div class="col-xs-12">
-                      7 Events
+                      <p class="course_contents course_location">
+                        7 Events
+                      </p>
                     </div>
                   </div>
                 </div>
-                <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
+                <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1 percentage-complete">
                   0%
                 </div>
               </div>
               <hr>
               <div class="row">
                 <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
-                  <div class="row">
-                    <div class="col-xs-12">
-                      DUE
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-xs-12">
-                      NA
+                  <div class="pull-left">
+                    <div class="calendar">
+                      <p class="calendar_month">DUE</p>
+                      <p class="calendar_date">NA</p>
                     </div>
                   </div>
                 </div>
                 <div class="col-xs-6 col-sm-8 col-md-10 col-lg-10">
                   <div class="row">
                     <div class="col-xs-12">
-                      <a href="show-ordered.php">Name Of Resident Assignment</a>
+                      <h3 class="course_description">
+                        <a href="show-ordered.php">Name Of Resident Assignment</a>
+                      </h3>
                     </div>
                   </div>
                   <div class="row">
                     <div class="col-xs-12">
-                      3 Courses, 1 Event
+                      <p class="course_contents course_location">
+                        3 Courses, 1 Event
+                      </p>
                     </div>
                   </div>
                 </div>
-                <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1">
+                <div class="col-xs-3 col-sm-2 col-md-1 col-lg-1 percentage-complete">
                   0%
                 </div>
               </div>
