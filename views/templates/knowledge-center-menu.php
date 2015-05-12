72edb5f649c4d0b92e007c591cec9b771028e4be
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2015-05-12 13:01:25 -0500

    ordered show modal

---
+++ views/templates/knowledge-center-menu.php
@@ -0,0 +1,10 @@
+<ul class="dropdown-menu">
+  <li><a href="#">General Principles And Fundamentals</a></li>
+  <li><a href="#">Orthopaedics</a></li>
+  <li><a href="#">Neurological Surgery</a></li>
+  <li><a href="#">Otolaryngology</a></li>
+  <li><a href="#">Oral Maxillofacial Surgery</a></li>
+  <li><a href="#">Plastic And Reconstructive Surgery</a></li>
+  <li><a href="#">Podiatric Medicine</a></li>
+  <li><a href="#">Veterinary Medicine</a></li>
+</ul>
