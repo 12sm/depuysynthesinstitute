292400ab9f925f585b1a183ab1c247c8204ea614
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2015-05-13 14:53:07 -0500

    single material

--- views/my-assignments/chief.php
+++ views/my-assignments/chief.php
@@ -8,15 +8,41 @@
         <?php include '../templates/logo-search-sm.php';?>
         <?php include '../templates/nav-sm.php';?>
       </div>
-
-
       <div class="right_column col-xs-12 col-sm-8 col-md-9">
         <?php include '../templates/mobile-header.php';?>
 
-        CONTENT GOES HERE
+
+
+        <div id="smt_col1Wrapper">
+          <div id="smt_col1">
+
+
+            <div class="modSpacer">
+              <div class="row">
+                <div class="col-xs-12">
+                  <a href="#">
+                    <h1>
+                      Content
+                    </h1>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div class="modSpacer">
+            </div>
+
+            <div class="modSpacer">
+            </div>
+
+
+          </div>
+        </div>
+
+
+
 
         <?php include '../templates/footer.php';?>
-
       </div>
     </div>
     <?php include '../templates/scripts.php';?>
--- views/my-assignments/single.php
+++ views/my-assignments/single.php
@@ -19,13 +19,248 @@
 
             <div class="modSpacer">
               <div class="row">
-                <div class="col-xs-12">
-                  <img src="">
+                <div class="pho-show col-xs-12">
+                  <img src="http://localhost/~Prometheus_Boombox/depuy-institute/img/eLearning-eModule.png" height="auto" width="100%">
+                </div>
+              </div>
+              <div class="row">
+                <div class="pho-show col-xs-10">
+                  <h1>
+                    ORTHOCORD Suture and Arthroscopic Knot Tying CA#13038B/DJ12808A
+                  </h1>
+                </div>
+                <div class="pho-show col-xs-2">
+                  Star Banner Icon
+                </div>
+              </div>
+              <div class="row">
+                <div class="pho-show col-xs-12">
+                  <h3>
+                    eLearning, 50 credits
+                  </h3>
+                </div>
+              </div>
+              <div class="row">
+                <div class="pho-show col-xs-12">
+                  Share Button | 879 views
+                </div>
+              </div>
+              <div class="row">
+                <div class="pho-show col-xs-12">
+                  <hr>
+                </div>
+              </div>
+              <div class="row">
+                <div class="pho-show col-xs-12">
+                  <h3>
+                    eLearning, 50 creditsWith 19 lab stations, six training rooms and eight conference rooms, the 75,000 square foot DePuy Institute is the largest facility of its kind on the East Coast. We work with a diverse group of educators, institutions and societies from around the world to create nearly 300 high-quality courses annually.
+                  </h3>
+                </div>
+              </div>
+              <div class="row">
+                <div class="pho-show col-xs-12">
+                  <h2>
+                    Launch eLearning
+                  </h2>
+                </div>
+              </div>
+              <div class="row">
+                <div class="pho-show col-xs-12">
+                  <hr>
                 </div>
               </div>
             </div>
 
             <div class="modSpacer">
+              <div class="row">
+                <div class="col-xs-12">
+                  <h1>
+                    Related eLearning
+                  </h1>
+                </div>
+              </div>
+              <div class="row">
+                <div class="pho-show col-xs-12 col-sm-6 col-md-3">
+                  <div class="photo-fill row">
+                    <div class="col-xs-9">
+                      <h3>
+                        Orthocord High Strength Orthopaedic Suture and Arthroscopic Knot Tying
+                      </h3>
+                    </div>
+                    <div class="col-xs-3">
+                      Star Banner Icon
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h5>
+                        Orthopaedics
+                      </h5>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h3>
+                        ORTHOCORD Suture and Anthroscopic Knot Tying CA#13038B/DJ12808A
+                      </h3>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h3>
+                        <h6>
+                          eLearning, 50 credits
+                        </h6>
+                      </h3>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <hr>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="pho-show col-xs-12">
+                      Share Button | 879 views
+                    </div>
+                  </div>
+                </div>
+                <div class="pho-show col-xs-12 col-sm-6 col-md-3">
+                  <div class="photo-fill row">
+                    <div class="col-xs-9">
+                      <h3>
+                        Orthocord High Strength Orthopaedic Suture and Arthroscopic Knot Tying
+                      </h3>
+                    </div>
+                    <div class="col-xs-3">
+                      Star Banner Icon
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h5>
+                        Orthopaedics
+                      </h5>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h3>
+                        ORTHOCORD Suture and Anthroscopic Knot Tying CA#13038B/DJ12808A
+                      </h3>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h3>
+                        <h6>
+                          eLearning, 50 credits
+                        </h6>
+                      </h3>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <hr>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="pho-show col-xs-12">
+                      Share Button | 879 views
+                    </div>
+                  </div>
+                </div>
+                <div class="pho-show col-xs-12 col-sm-6 col-md-3">
+                  <div class="photo-fill row">
+                    <div class="col-xs-9">
+                      <h3>
+                        Orthocord High Strength Orthopaedic Suture and Arthroscopic Knot Tying
+                      </h3>
+                    </div>
+                    <div class="col-xs-3">
+                      Star Banner Icon
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h5>
+                        Orthopaedics
+                      </h5>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h3>
+                        ORTHOCORD Suture and Anthroscopic Knot Tying CA#13038B/DJ12808A
+                      </h3>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h3>
+                        <h6>
+                          eLearning, 50 credits
+                        </h6>
+                      </h3>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <hr>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="pho-show col-xs-12">
+                      Share Button | 879 views
+                    </div>
+                  </div>
+                </div>
+                <div class="pho-show col-xs-12 col-sm-6 col-md-3">
+                  <div class="photo-fill row">
+                    <div class="col-xs-9">
+                      <h3>
+                        Orthocord High Strength Orthopaedic Suture and Arthroscopic Knot Tying
+                      </h3>
+                    </div>
+                    <div class="col-xs-3">
+                      Star Banner Icon
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h5>
+                        Orthopaedics
+                      </h5>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h3>
+                        ORTHOCORD Suture and Anthroscopic Knot Tying CA#13038B/DJ12808A
+                      </h3>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h3>
+                        <h6>
+                          eLearning, 50 credits
+                        </h6>
+                      </h3>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <hr>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="pho-show col-xs-12">
+                      Share Button | 879 views
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
 
             <div class="modSpacer">
